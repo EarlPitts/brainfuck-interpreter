cc0d27b2525a3e938a00db7dd189aae5b9d261d1
Bendeguz Seres <grafitpor@gmail.com>
2023-07-27 14:05:24 +0200

    Implement jumps

--- Brainfuck.hs
+++ Brainfuck.hs
@@ -1,9 +1,8 @@
 module Brainfuck where
 
-import Debug.Trace
-
 import Control.Monad.State as ST
 import Data.Either
+import Debug.Trace
 import Text.Parsec
 import Text.Parsec.String
 
@@ -46,41 +45,62 @@ decMemory :: [Int] -> Int -> [Int]
 decMemory m i = take i m ++ [pred (m !! i)] ++ drop (i + 1) m
 
 incIp :: VM -> VM
-incIp s = s { ip = succ (ip s) }
+incIp s = s {ip = succ (ip s)}
 
 currentByte :: VM -> Int
 currentByte s = memory s !! dp s
 
-evalStep :: Stmt -> VM -> VM
-evalStep INC s   = incIp $ s {dp = succ (dp s)}
-evalStep DEC s   = incIp $ s {dp = pred (dp s), ip = succ (ip s)}
-evalStep INCB s  = incIp $ s {memory = incMemory (memory s) (dp s)}
-evalStep DECB s  = incIp $ s {memory = decMemory (memory s) (dp s)}
--- evalStep JEZ s p  = if currentByte s == 0 then jumpForward p (ip s) else s
--- evalStep JNZ s p  = if currentByte s /= 0 then jumpBackward p (ip s) else s
+evalStep :: Stmt -> VM -> Program -> VM
+evalStep INC s _ = s {dp = succ (dp s)}
+evalStep DEC s _ = s {dp = pred (dp s)}
+evalStep INCB s _ = s {memory = incMemory (memory s) (dp s)}
+evalStep DECB s _ = s {memory = decMemory (memory s) (dp s)}
+evalStep JEZ s p = if currentByte s == 0 then s { ip = jumpForward p (ip s) } else s
+evalStep JNZ s p = if currentByte s /= 0 then s { ip = jumpBackward p (ip s) } else s
 
-jumpBack :: Program -> Int -> Int
-jumpBack p i = undefined
+jumpBackward :: Program -> Int -> Int
+jumpBackward p i = evalState (go p (i - 1)) 0
+  where
+    go p i = do
+      case p !! i of
+        JEZ -> do
+          n <- get
+          if n == 0 then return i else put (n - 1) >> go p (i - 1)
+        JNZ -> do
+          n <- get
+          put (n + 1)
+          go p (i - 1)
+        _ -> go p (i - 1)
 
 jumpForward :: Program -> Int -> Int
-jumpForward p i = undefined
+jumpForward p i = evalState (go p (i + 1)) 0
+  where
+    go p i = do
+      case p !! i of
+        JNZ -> do
+          n <- get
+          if n == 0 then return i else put (n - 1) >> go p (i + 1)
+        JEZ -> do
+          n <- get
+          put (n + 1)
+          go p (i + 1)
+        _ -> go p (i + 1)
 
 eval :: Program -> ST.State VM ()
 eval is = do
   m <- get
-  if length is <= ip m
+  if length is <= ip (trace (show m) m)
     then return ()
     else do
       let i = is !! ip m
-      -- if isJump i
-      -- then do
-      -- else
-      let newState = evalStep i m
-      put $ newState { ip = succ (ip newState) }
+      put $ incIp (evalStep i m is)
       eval is
 
 initState = VM {dp = 0, ip = 0, memory = replicate 10 0}
 
-p' = case parse p "" "->+<" of
-  Right is -> is
-  Left _ -> error "Noo"
+run :: String -> VM
+run is = execState (eval prog) initState
+  where prog = case parse p "" is of
+                  Right is -> is
+                  Left _ -> error "Noo"
+  
