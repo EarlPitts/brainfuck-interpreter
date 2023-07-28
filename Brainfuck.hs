f7ddf38e44a57faf8243e22fbd103420937de566
Bendeguz Seres <grafitpor@gmail.com>
2023-07-28 16:40:38 +0200

    Implement IO

--- Brainfuck.hs
+++ Brainfuck.hs
@@ -1,21 +1,26 @@
 module Brainfuck where
 
-import Data.Word
 import Control.Monad.State as ST
 import Data.Either
 import Data.List
+import Data.ByteString.Internal
 import Data.Maybe
+import Data.Word
 import Debug.Trace
 import Text.Parsec
 import Text.Parsec.String
+import System.IO
+import System.Environment
 
-data Instr
+data Token
   = JEZ
   | JNZ
   | INC
   | DEC
   | INCB
   | DECB
+  | OUT
+  | IN
   deriving (Show, Eq)
 
 data Jump
@@ -30,23 +35,36 @@ data Stmt
   | DecB
   deriving (Show, Eq)
 
-toInstr :: Char -> Instr
-toInstr '>' = INC
-toInstr '<' = DEC
-toInstr '[' = JEZ
-toInstr ']' = JNZ
-toInstr '+' = INCB
-toInstr '-' = DECB
+data IOStmt
+  = In
+  | Out
+  deriving (Show, Eq)
 
-instr :: Parser Instr
-instr = toInstr <$> oneOf "><+-[]"
+data Instr
+  = IOStmt IOStmt
+  | Jump Jump
+  | Stmt Stmt
+  deriving (Show, Eq)
 
-type RawProgram = [Instr]
+toToken :: Char -> Token
+toToken '>' = INC
+toToken '<' = DEC
+toToken '[' = JEZ
+toToken ']' = JNZ
+toToken '+' = INCB
+toToken '-' = DECB
+toToken '.' = OUT
+toToken ',' = IN
 
-type Program = [Either Jump Stmt]
+instr :: Parser Token
+instr = toToken <$> oneOf "><+-[],."
 
-p :: Parser [Instr]
-p = spaces *> sepBy instr spaces <* spaces
+type RawProgram = [Token]
+
+type Program = [Instr]
+
+p :: Parser [Token]
+p = spaces *> sepEndBy instr spaces <* spaces
 
 toProgram :: RawProgram -> Program -- Should be a (Maybe Program) (unbalanced jumps)
 toProgram is = fromRaw pairs <$> numbered
@@ -54,16 +72,18 @@ toProgram is = fromRaw pairs <$> numbered
     numbered = zip [1 ..] is
     pairs = pairJumps $ filter (\i -> snd i `elem` [JNZ, JEZ]) numbered
 
-fromRaw :: [(Int, Int)] -> (Int, Instr) -> Either Jump Stmt
-fromRaw ps (n, JEZ) = Left (Jez (fromJust (lookup n ps)))
-fromRaw ps (n, JNZ) = Left (Jnz (fromJust (lookup n ps)))
-fromRaw ps (n, INC) = Right Inc
-fromRaw ps (n, DEC) = Right Dec
-fromRaw ps (n, INCB) = Right IncB
-fromRaw ps (n, DECB) = Right DecB
+fromRaw :: [(Int, Int)] -> (Int, Token) -> Instr
+fromRaw ps (n, JEZ)  = Jump (Jez (fromJust (lookup n ps)))
+fromRaw ps (n, JNZ)  = Jump (Jnz (fromJust (lookup n ps)))
+fromRaw ps (n, INC)  = Stmt Inc
+fromRaw ps (n, DEC)  = Stmt Dec
+fromRaw ps (n, INCB) = Stmt IncB
+fromRaw ps (n, DECB) = Stmt DecB
+fromRaw ps (n, IN)   = IOStmt In
+fromRaw ps (n, OUT)  = IOStmt Out
 
 -- Finds each pair of jumps and pairs them together
-pairJumps :: [(Int, Instr)] -> [(Int, Int)]
+pairJumps :: [(Int, Token)] -> [(Int, Int)]
 pairJumps js = concatMap (\((i, _), (i', _)) -> [(i, i'), (i', i)]) (go js [])
   where
     go [] s = []
@@ -83,6 +103,9 @@ incMemory m i = take i m ++ [succ (m !! i)] ++ drop (i + 1) m
 decMemory :: [Word8] -> Int -> [Word8]
 decMemory m i = take i m ++ [pred (m !! i)] ++ drop (i + 1) m
 
+modMemory :: [Word8] -> Int -> Word8 -> [Word8]
+modMemory m i n = take i m ++ [n] ++ drop (i + 1) m
+
 incIp :: VM -> VM
 incIp s = s {ip = succ (ip s)}
 
@@ -99,6 +122,16 @@ evalStmt Dec s = s {dp = pred (dp s)}
 evalStmt IncB s = s {memory = incMemory (memory s) (dp s)}
 evalStmt DecB s = s {memory = decMemory (memory s) (dp s)}
 
+evalIOStmt :: IOStmt -> VM -> IO VM
+evalIOStmt In s = do
+  input <- getChar
+  let n = c2w input 
+  return $ s {memory = modMemory (memory s) (dp s) n}
+evalIOStmt Out s = do
+  let n = w2c $ currentByte s
+  putChar n
+  return s
+
 eval :: Program -> ST.StateT VM IO ()
 eval is = do
   m <- get
@@ -108,14 +141,18 @@ eval is = do
       let i = is !! ip m
       liftIO $ print m
       case i of
-        Left j -> do
+        Jump j -> do
           put $ incIp (evalJump j m)
           eval is
-        Right s -> do
+        Stmt s -> do
           put $ incIp (evalStmt s m)
           eval is
+        IOStmt ios -> do
+          newState <- liftIO $ evalIOStmt ios m
+          put $ incIp newState
+          eval is
 
-initState = VM {dp = 0, ip = 0, memory = replicate 10 0}
+initState = VM {dp = 0, ip = 0, memory = replicate 100 0}
 
 run :: String -> IO VM
 run is = execStateT (eval prog) initState
@@ -123,3 +160,10 @@ run is = execStateT (eval prog) initState
     prog = case parse p "" is of
       Right is -> toProgram is
       Left _ -> error "Noo"
+
+main :: IO VM
+main = do
+  args <- getArgs
+  source <- readFile (head args)
+  print source
+  run source
